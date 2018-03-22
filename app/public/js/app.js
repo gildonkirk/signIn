075fc9f0d2f548168a8fc3eb8c41126f350908b0
Gildon Kirk <gildonkirk@gmail.com>
2018-03-22 10:14:08 -0400

    users info shows correctly in browser console

---
+++ app/public/js/app.js
@@ -0,0 +1,13 @@
+$(document).on('click', '.newUserSubmit', function(event) {
+  event.preventDefault();
+  let newEmail = $('.newUserEmail').val().trim();
+  let newName = $('.newUserName').val().trim();
+  let newAge = $('.newUserAge').val().trim();
+  let newUser = {
+    email: newEmail,
+    name: newName,
+    age: parseInt(newAge)
+  };
+
+  console.log(newUser);
+});
