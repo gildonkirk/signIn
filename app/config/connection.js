5eccc1fd06fbba55e495af5035455ef62b0b8a80
Gildon Kirk <gildonkirk@gmail.com>
2018-03-22 10:38:12 -0400

    syntax change to connection

--- app/config/connection.js
+++ app/config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("sign_in_db", "root", "certifiedG48*", {
+var sequelize = new Sequelize("sign_in_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
   pool: {
